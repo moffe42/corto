4564b79538e9584baa2c992f8a302f636e6d5f09
freek <freek@ruc.dk>
2010-10-10 17:31:32 +0000

    Added support for urn:oasis:names:tc:SAML:metadata:attribute

--- library/Corto/XmlToArray.php
+++ library/Corto/XmlToArray.php
@@ -18,31 +18,31 @@ class Corto_XmlToArray_Exception extends Corto_ProxyServer_Exception {}
  * @version    $Id:$
  */
 
-class Corto_XmlToArray
-{
-    const PRIVATE_KEY_PREFIX    = '__';
-    const TAG_NAME_KEY          = '__t';
-    const VALUE_KEY             = '__v';
-    const PLACEHOLDER_VALUE     = '__placeholder__';
-    const ATTRIBUTE_KEY_PREFIX  = '_';
-    const MAX_RECURSION_LEVEL   = 50;
+class Corto_XmlToArray {
+    const PRIVATE_KEY_PREFIX = '__';
+    const TAG_NAME_KEY = '__t';
+    const VALUE_KEY = '__v';
+    const PLACEHOLDER_VALUE = '__placeholder__';
+    const ATTRIBUTE_KEY_PREFIX = '_';
+    const MAX_RECURSION_LEVEL = 50;
 
     /**
      * @var array All namespaces used in SAML2 messages.
      */
     protected static $_namespaces = array(
-        'urn:oasis:names:tc:SAML:1.0:protocol'          => 'samlp',
-        'urn:oasis:names:tc:SAML:1.0:assertion'         => 'saml',
-        'urn:oasis:names:tc:SAML:2.0:protocol'          => 'samlp',
-        'urn:oasis:names:tc:SAML:2.0:assertion'         => 'saml',
-        'urn:oasis:names:tc:SAML:2.0:metadata'          => 'md',
-        'http://www.w3.org/2001/XMLSchema-instance'     => 'xsi',
-        'http://www.w3.org/2001/XMLSchema'              => 'xs',
-        'http://schemas.xmlsoap.org/soap/envelope/'     => 'SOAP-ENV',
-        'http://www.w3.org/2000/09/xmldsig#'            => 'ds',
-        'http://www.w3.org/2001/04/xmlenc#'             => 'xenc',
-        'corto.wayf.dk'			                        => 'corto',
-     );
+        'urn:oasis:names:tc:SAML:1.0:protocol' => 'samlp',
+        'urn:oasis:names:tc:SAML:1.0:assertion' => 'saml',
+        'urn:oasis:names:tc:SAML:2.0:protocol' => 'samlp',
+        'urn:oasis:names:tc:SAML:2.0:assertion' => 'saml',
+        'urn:oasis:names:tc:SAML:2.0:metadata' => 'md',
+        'urn:oasis:names:tc:SAML:metadata:attribute' => 'mdattr',
+        'http://www.w3.org/2001/XMLSchema-instance' => 'xsi',
+        'http://www.w3.org/2001/XMLSchema' => 'xs',
+        'http://schemas.xmlsoap.org/soap/envelope/' => 'SOAP-ENV',
+        'http://www.w3.org/2000/09/xmldsig#' => 'ds',
+        'http://www.w3.org/2001/04/xmlenc#' => 'xenc',
+        'corto.wayf.dk' => 'corto',
+    );
 
     /**
      * @var array All XML entities which are treated as single values in Corto.
@@ -63,10 +63,11 @@ class Corto_XmlToArray
 #        'md:RoleDescriptor',
 #        'md:SPSSODescriptor',
         'md:SurName',
+        'mdattr:EntityAttributes',
         'saml:Advice',
-        'saml:Assertion',             #
-        'saml:AssertionIDRef',        #
-        'saml:AssertionURIRef',        #
+        'saml:Assertion', #
+        'saml:AssertionIDRef', #
+        'saml:AssertionURIRef', #
 #        'saml:Attribute',
 #        'saml:AttributeStatement',
         'saml:Audience',
@@ -75,12 +76,12 @@ class Corto_XmlToArray
         'saml:AuthnContextClassRef',
         'saml:AuthnContextDecl',
         'saml:AuthnContextDeclRef',
-        'saml:AuthnStatement',        #
+        'saml:AuthnStatement', #
 #        'saml:AuthzDecisionStatement',
         'saml:BaseID',
 #        'saml:Condition',
         'saml:Conditions',
-        'saml:EncryptedAssertion',    #
+        'saml:EncryptedAssertion', #
 #        'saml:EncryptedAttribute',
         'saml:EncryptedID',
         'saml:Evidence',
@@ -127,8 +128,8 @@ class Corto_XmlToArray
         'ds:Transforms',
         'ds:X509Data',
         'ds:X509Certificate',
-		'corto:IDPList',
-);
+        'corto:IDPList',
+    );
 
     public static function xml2array($xml)
     {
@@ -138,8 +139,8 @@ class Corto_XmlToArray
         if ($parserResultStatus !== 1) {
             throw new Corto_XmlToArray_Exception(
                 'Error parsing incoming XML. ' . PHP_EOL .
-                'Error code: '.xml_error_string(xml_get_error_code($parser)) . PHP_EOL .
-                'XML: ' . $xml);
+                        'Error code: ' . xml_error_string(xml_get_error_code($parser)) . PHP_EOL .
+                        'XML: ' . $xml);
         }
         xml_parser_free($parser);
         self::$_singulars = array_fill_keys(self::$_singulars_list, 1);
@@ -156,28 +157,27 @@ class Corto_XmlToArray
      * @param array $namespaceMapping
      * @return array
      */
-     
+
     protected static $i = 0;
-     
+
     protected static function _xml2array(&$elements, $level = 1, $namespaceMapping = array())
     {
         static $defaultNs;
         if ($level == 1) {
-                self::$i = 0;
-                $defaultNs = '';
+            self::$i = 0;
+            $defaultNs = '';
         }
         $newElement = array();
-        while(isset($elements[self::$i]) && $value = $elements[self::$i++]) {
+        while (isset($elements[self::$i]) && $value = $elements[self::$i++]) {
             if ($value['type'] == 'close') {
                 return $newElement;
             } elseif ($value['type'] == 'cdata') {
                 continue;
             }
-
             $hashedAttributes = array();
             $tagName = $value['tag'];
             if (isset($value['attributes']) && $attributes = $value['attributes']) {
-                foreach($attributes as $attributeKey => $attributeValue) {
+                foreach ($attributes as $attributeKey => $attributeValue) {
                     unset($attributes[$attributeKey]);
 
                     if (preg_match("/^xmlns:(.+)$/", $attributeKey, $namespacePrefixAndTag)) {
@@ -185,11 +185,11 @@ class Corto_XmlToArray
                             self::$_namespaces[$attributeValue] = $namespacePrefixAndTag[1];
                         }
                         $namespaceMapping[$namespacePrefixAndTag[1]] = self::$_namespaces[$attributeValue];
-                        $hashedAttributes['_xmlns:'.self::$_namespaces[$attributeValue]] = $attributeValue;
+                        $hashedAttributes['_xmlns:' . self::$_namespaces[$attributeValue]] = $attributeValue;
                     } elseif (preg_match("/^xmlns$/", $attributeKey)) {
                         $defaultNs = self::$_namespaces[$attributeValue];
-                        $hashedAttributes['_xmlns:'.$defaultNs] = $attributeValue;
-                   } else {
+                        $hashedAttributes['_xmlns:' . $defaultNs] = $attributeValue;
+                    } else {
                         $hashedAttributes[self::ATTRIBUTE_KEY_PREFIX . $attributeKey] = $attributeValue;
                     }
                 }
@@ -204,12 +204,12 @@ class Corto_XmlToArray
             if ($hashedAttributes) {
                 $complete = array_merge($complete, $hashedAttributes);
             }
-            if (isset($value['value']) && $attributeValue = trim($value['value'])) {
-                $complete[self::VALUE_KEY] = $attributeValue;
+            if (isset($value['value'])) {
+                $complete[self::VALUE_KEY] = $attributeValue = trim($value['value']);
             }
             if ($value['type'] == 'open') {
                 $cs = self::_xml2array($elements, $level + 1, $namespaceMapping);
-                foreach($cs as $c) {
+                foreach ($cs as $c) {
                     $tagName = $c[self::TAG_NAME_KEY];
                     unset($c[self::TAG_NAME_KEY]);
                     if (!isset(self::$_singulars[$tagName])) {
@@ -239,7 +239,7 @@ class Corto_XmlToArray
      * @param string $elementName Specific element to convert, if empty then the top level element is used
      * @return string XML from array
      */
-    public static function array2xml(array $hash, $elementName = "", $useIndentation=false)
+    public static function array2xml(array $hash, $elementName = "", $useIndentation = false)
     {
         $writer = new XMLWriter();
         $writer->openMemory();
@@ -265,7 +265,7 @@ class Corto_XmlToArray
     protected static function _array2xml($hash, $elementName, XMLWriter $writer, $level = 0)
     {
         if ($level > self::MAX_RECURSION_LEVEL) {
-            throw new Exception('Recursion threshold exceed on element: '.$elementName . ' for hashvalue: ' . var_export($hash, true));
+            throw new Exception('Recursion threshold exceed on element: ' . $elementName . ' for hashvalue: ' . var_export($hash, true));
         }
         if ($hash == self::PLACEHOLDER_VALUE) {
             // Ignore placeholders
@@ -275,7 +275,7 @@ class Corto_XmlToArray
             $writer->startElement($elementName);
         }
 
-        foreach((array)$hash as $key => $value) {
+        foreach ((array) $hash as $key => $value) {
             if (is_int($key)) {
                 // Normal numeric index, value is probably a hash structure, recurse...
                 self::_array2xml($value, $elementName, $writer, $level + 1);
@@ -302,7 +302,7 @@ class Corto_XmlToArray
     public static function attributes2array($attributes)
     {
         $res = array();
-        foreach((array)$attributes as $attribute) {
+        foreach ((array) $attributes as $attribute) {
             foreach ($attribute['saml:AttributeValue'] as $value) {
                 $res[$attribute['_Name']][] = $value[self::VALUE_KEY];
             }
@@ -313,16 +313,16 @@ class Corto_XmlToArray
     public static function array2attributes($attributes)
     {
         $res = array();
-        foreach((array)$attributes as $name => $attribute) {
+        foreach ((array) $attributes as $name => $attribute) {
             $newAttribute = array(
                 '_Name' => $name,
                 '_NameFormat' => 'urn:oasis:names:tc:SAML:2.0:attrname-format:basic',
             );
-            foreach ((array)$attribute as $value) {
-                $newAttribute['saml:AttributeValue'][] = array (
-                   '_xmlns:xsi'     => 'http://www.w3.org/2001/XMLSchema-instance',
-                   '_xsi:type'      => 'xs:string',
-                   self::VALUE_KEY  => $value,
+            foreach ((array) $attribute as $value) {
+                $newAttribute['saml:AttributeValue'][] = array(
+                    '_xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+                    '_xsi:type' => 'xs:string',
+                    self::VALUE_KEY => $value,
                 );
             }
             $res[] = $newAttribute;
