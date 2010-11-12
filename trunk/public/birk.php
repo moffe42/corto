d6b7d6c6a41f79146b4b6dfd584077327628d73b
freek@ruc.dk <freek@ruc.dk@1b63ba68-5496-7452-b384-8c9abc699c3d>
2010-11-12 22:00:25 +0000

    tmp checkins for birk poc.

--- trunk/public/birk.php
+++ trunk/public/birk.php
@@ -25,10 +25,9 @@ try {
 /*
  * If called without path - use demo.php
  */
-    $metadatafile = '/var/tmp/birk_optimized_metadata.php';
+    $metadatafile = '../configs/birk_optimized_metadata.php';
 
     if (empty($_SERVER['PATH_INFO']) || $_SERVER['PATH_INFO'] == '/moc.elgoog-ACS'|| $_SERVER['PATH_INFO'] == '/ude@evil-ACS') {
-        updatemetadata($metadatafile);
         $server->setMetadata($metadatafile);
         $idplist = $server->getAllowedIDPs('testing', true);
 
@@ -137,84 +136,4 @@ function render($template, $vars = array()) {
     $content = ob_get_contents(); // Get the content of the buffer
     ob_end_clean(); // End buffering and discard
     return $content; // Return the content
-}
-
-function updatemetadata($metadatafile) {
-    require '../library/Corto/Module/Metadata.php';
-
-    $meta = new Corto_Module_Metadata();
-    $remotemd = 'http://janus-dev.test.wayf.dk/module.php/janus/exportentities.php?type[]=saml20-idp&state=prodaccepted&external=corto';
-    $md = Corto_XmlToArray::xml2array(file_get_contents($remotemd), true);
-
-    $metadatasources = array(
-        'federations' => array(
-            'testing' => array(
-                'public' => array(
-                    'php:' . dirname(__FILE__) . '/../configs/birk.meta.php',
-                    $md,
-                ),
-            ),
-        ),
-    );
-
-
-    $meta->prepareMetadata($metadatasources, $metadatafile);
-
-    $md = eval(file_get_contents($metadatafile));
-    foreach ($md['federations']['testing'] as $id => $entity) {
-        unset($entity['original']);
-        if (nvl3($entity, 'IDP', 'corto:IDPList', 0) != 'https://wayf.wayf.dk') {
-            $entities[$id] = $entity;
-            continue;
-        } else {
-            $newid = preg_replace("/^(_HOSTED_)/", '$1/proxy', $id);
-            $entity['corto:sharedkey'] = array(
-                array('__v' => 'abrakadabra'),
-            );
-            $newentity = $entity;
-            $newentity['IDP']['corto:IDPList'] = array('_HOSTED_/wayf.wayf.dk', $id);
-            $newentity['entityID'] = $newid;
-            $sso = nvl3($newentity['IDP'], 'SingleSignOnService', 0, 'Location');
-
-            $asc = $sso . "/ACS";
-            $entity['SP'] = array(
-                'AssertionConsumerService' =>
-                array(
-                    1 =>
-                    array(
-                        'Location' => $asc,
-                        'Binding' => 'JSON-Redirect',
-                    ),
-                    'default' => 1,
-                ),
-            );
-
-            $lookuptablextra[$asc] = array(
-                'EntityID' => $id,
-                'Service' => 'AssertionConsumerService',
-                'Binding' => 'JSON-Redirect',
-            );
-
-
-            $sso = preg_replace("/^(_HOSTED_)/", '$1/proxy', $sso);
-            $newentity['IDP']['SingleSignOnService'][0]['Location'] = $sso;
-
-            $lookuptablextra[$sso] = array(
-                'EntityID' => $newid,
-                'Service' => 'SingleSignOnService',
-                'Binding' => 'JSON-Redirect',
-            );
-
-
-            $entities[$newid] = $newentity;
-
-            unset($entity['IDP']['corto:IDPList']);
-            $entity['IDP']['corto:IDPList'][0] = '_COHOSTED_/null.php';
-            $entities[$id] = $entity;
-        }
-    }
-
-    $export = array('federations' => array('testing' => $entities), 'lookuptable' => array('testing' => array_merge($md['lookuptable']['testing'], $lookuptablextra)));
-    file_put_contents($metadatafile . '.tmp', "return " . var_export($export, true) . ";");
-    @rename($metadatafile . '.tmp', $metadatafile);
 }
