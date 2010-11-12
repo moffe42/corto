cbfd18c82c25ffa227d7c79ec02a8683bcd7219b
freek <freek@ruc.dk>
2010-11-12 10:48:01 +0000

    In progress commit:
    Metadata only configuration.
    Autogeneration of Metadata services at a well known (ie. entity id url) location.
    Separation of generation of optimized metadata from handling of requests.

--- filters/DemoFilter.php
+++ filters/DemoFilter.php
@@ -7,6 +7,74 @@
  * To change this template use File | Settings | File Templates.
  */
 
+class StdSingleLogonService {
+
+    static function sso()
+    {
+        $request = $_POST['cortodata']['request'];
+        $server = $_POST['cortodata']['server'];
+        $params = $_POST['cortodata']['params'];
+
+
+        // If we configured an IDPList in metadata this is our primary scoping
+        $scopedIDPs = $server->getPresetIDPs();
+
+        /**
+         * Add scoping in request to configured scoping - this is NOT according to the spec
+         * which says that you MUST append to a received IDPList
+         */
+        
+        foreach ((array) nvl3($request, 'samlp:Scoping', 'samlp:IDPList', 'samlp:IDPEntry') as $IDPEntry) {
+            $scopedIDPs[] = $IDPEntry['_ProviderID'];
+        }
+
+        $requesterIDs = array($params['EntityID'], $request['saml:Issuer']['__v']);
+
+        // filter out already visited proxies (RequesterID) to prevent looping ...
+        foreach ((array) nvl2($request, 'samlp:Scoping', 'samlp:RequesterID') as $requesterID) {
+            $requesterIDs[] = $requesterID['__v'];
+        }
+        // remove issuer + us from scope for use now ..
+
+        $relevantScopedIDPs = array_diff($scopedIDPs, $requesterIDs);
+
+        // Get all registered Single Sign On Services
+        $candidateIDPs = $server->getAllowedIdps();
+
+        // If we have scoping, filter out every non-scoped IdP
+        $candidateIDPs = empty($relevantScopedIDPs) ? $candidateIDPs : array_intersect($relevantScopedIDPs, $candidateIDPs);
+
+        // 1+ candidate found and 1+ in scoped, send authentication request to the first one
+        if ($idp = array_shift($candidateIDPs)) {
+            return $server->sendAuthenticationRequest($request, $idp, $relevantScopedIDPs);
+        }
+
+        // No IdPs found! Send an error response back.
+        if (empty($candidateIDPs)) {
+            $server->getSessionLog()->debug("SSO: No Supported Idps!");
+            $response = $server->createErrorResponse($request, 'NoSupportedIDP');
+            return $server->sendResponseToRequestIssuer($request, $response);
+        }
+
+        // 1+ non-scoped IdPs found, but isPassive attribute given, unable to continue
+        // NO call wayf first it might have an idea ...
+        if (nvl($request, '_IsPassive')) {
+            $server->getSessionLog()->debug("SSO: IsPassive with multiple IdPs!");
+            $response = $server->createErrorResponse($request, 'NoPassive');
+            return $server->sendResponseToRequestIssuer($request, $response);
+        }
+
+        // Store the request in the session
+        $id = $request['_ID'];
+        $_SESSION[$id]['SAMLRequest'] = $request;
+
+        // Show WAYF
+        $server->getSessionLog()->debug("SSO: Showing WAYF");
+        return showWayf($request, $candidateIDPs);
+    }
+
+}
+
 class DemoFilterClass {
 
     static function demofilter()
@@ -20,35 +88,38 @@ class DemoFilterClass {
             $_SESSION['DemoFilterClassState'] = $_POST['cortodata'];
             ?>
             <html>
-            <head>
-                <body>
-                <form method=POST action="<?= $_POST['cortolocation'] ?>">
-                    <input type="submit" name="submit" value="Continue processing ...">
-                    <input type="hidden" name="cortopassthru" value="<?= $_POST['cortopassthru'] ?>">
-<p>This is a Corto "<?= $_POST['cortophase'] ?>" filter with user interaction. Just click to 'Continue' button to continue.
-</p>
-                </form>
-                 <pre>
+            <body>
+            <form method=POST action="<?= $_POST['cortolocation'] ?>"><input
+                    type="submit" name="submit" value="Continue processing ..."> <input
+                    type="hidden" name="cortopassthru"
+                    value="<?= $_POST['cortopassthru'] ?>">
+
+                <p>This is a Corto "<?= $_POST['cortophase'] ?>" filter with user
+                    interaction. Just click to 'Continue' button to continue.</p>
+            </form>
+<pre>
 <?= print_r($_POST['cortodata'], 1); ?>
                 </pre>
-                </body>
+            </body>
             </head>
             </html>
 
             <?php
             exit;
         }
-        $samlattribute = &$_SESSION['DemoFilterClassState']['receivedResponse']
+        $samlattribute = &$_SESSION['DemoFilterClassState']['response']
         ['saml:Assertion']['saml:AttributeStatement'][0]['saml:Attribute'];
         $attributes = attributes2array($samlattribute);
         $attributes['uid'][] = '-x-';
         $samlattribute = array2attributes($attributes);
 
+        $ret = $_SESSION['DemoFilterClassState'];
+        unset($_SESSION['DemoFilterClassState']);
         if ($_POST['cortoreturn'] == 'array') {
-            return $_SESSION['DemoFilterClassState'];
+            return $ret;
         } else {
             header('X-Corto-Return: true');
-            print json_encode($_SESSION['DemoFilterClassState']);
+            print json_encode($ret);
             exit;
         }
     }
