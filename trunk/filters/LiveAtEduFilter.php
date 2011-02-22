c343110efef71b6cd1ed280a7b4bbfb402b810d3
freek@ruc.dk <freek@ruc.dk@1b63ba68-5496-7452-b384-8c9abc699c3d>
2011-02-22 12:43:32 +0000

    Generic live@edu filter. Uses 'edupersonEntitlement' for telling the domain of the eppn:  'http://wayf.dk/live@edu/<domain>

--- trunk/filters/LiveAtEduFilter.php
+++ trunk/filters/LiveAtEduFilter.php
@@ -18,19 +18,25 @@ class LiveAtEduFilter {
         $dollar = array();
         preg_match("/^(.*)@/", $attributes['eduPersonPrincipalName'][0], $dollar);
 
-        $mail = $dollar[1];
+        $fid = $dollar[1];
+        foreach ($attributes['eduPersonEntitlement'] as $epn) {
+            if (preg_match("/^http:\\/\\/wayf\\.dk\\/live@edu\\/(.*)$/", $epn, $dollar)) {
+                $domain = $dollar[1];
+                break;
+            }
+        }
 
+        $newattributes['urn:oid:1.3.http://wayf.dk/live@edu/saml.live.wayf.dk6.1.4.1.5923.1.1.1.6'] = array('na@' . $domain);
         unset($assertion['saml:AttributeStatement']);
-        unset($assertion['ds:Signature']);
+        $assertion['saml:AttributeStatement'][0]['saml:Attribute'] = array2attributes($newattributes);
         unset($assertion['saml:AuthnStatement']['saml:AuthnContext']['saml:AuthenticatingAuthority']);
 
         $assertion['saml:Subject']['saml:NameID'] = Array(
-            '_Format' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-            '_SPNameQualifier' => "live.wayf.dk",
-            '__v' => $mail,
+            '_Format' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+            '__v' => $fid,
         );
 
-        $assertion['saml:Conditions']['saml:AudienceRestriction']['saml:Audience']['__v'] = $acs;
+        $assertion['saml:Conditions']['saml:AudienceRestriction']['saml:Audience']['__v'] = 'uri:WindowsLiveID';
         $assertion['saml:Subject']['saml:SubjectConfirmation']['saml:SubjectConfirmationData']['_Recipient'] = $acs;
 
         return $params['cortodata'];
