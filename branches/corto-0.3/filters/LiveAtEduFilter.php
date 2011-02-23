cdcb30db86d32719230787aa8d54db42a1425da0
jach@wayf.dk <jach@wayf.dk@1b63ba68-5496-7452-b384-8c9abc699c3d>
2011-02-23 09:14:19 +0000

    Merged r204 to 0.3 branch. Update to LiveAtEdu filter

--- branches/corto-0.3/filters/LiveAtEduFilter.php
+++ branches/corto-0.3/filters/LiveAtEduFilter.php
@@ -15,22 +15,34 @@ class LiveAtEduFilter {
         $assertion = &$params['cortodata']['response']['saml:Assertion'];
         $samlattribute = $assertion['saml:AttributeStatement'][0]['saml:Attribute'];
         $attributes = attributes2array($samlattribute);
+
+        // Get the federated id (ePPN))
         $dollar = array();
         preg_match("/^(.*)@/", $attributes['eduPersonPrincipalName'][0], $dollar);
+        $fid = $dollar[1];
 
-        $mail = $dollar[1];
+        // Get the WLID (eduPersonEntitlement)
+        $dollar = array();
+        foreach ($attributes['eduPersonEntitlement'] as $epe) {
+            if (preg_match("/^http:\\/\\/wayf\\.dk\\/live@edu\\/(.*)$/", $epe, $dollar)) {
+                $wlid = $dollar[1];
+                break;
+            }
+        }
 
+        // Send the WLID
+        $newattributes['urn:oid:1.3.6.1.4.1.5923.1.1.1.6'] = array($wlid);
         unset($assertion['saml:AttributeStatement']);
-        unset($assertion['ds:Signature']);
+        $assertion['saml:AttributeStatement'][0]['saml:Attribute'] = array2attributes($newattributes);
         unset($assertion['saml:AuthnStatement']['saml:AuthnContext']['saml:AuthenticatingAuthority']);
 
+        // Set subject to ePPN
         $assertion['saml:Subject']['saml:NameID'] = Array(
-            '_Format' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-            '_SPNameQualifier' => "live.wayf.dk",
-            '__v' => $mail,
+            '_Format' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+            '__v' => $fid,
         );
 
-        $assertion['saml:Conditions']['saml:AudienceRestriction']['saml:Audience']['__v'] = $acs;
+        $assertion['saml:Conditions']['saml:AudienceRestriction']['saml:Audience']['__v'] = 'uri:WindowsLiveID';
         $assertion['saml:Subject']['saml:SubjectConfirmation']['saml:SubjectConfirmationData']['_Recipient'] = $acs;
 
         return $params['cortodata'];
