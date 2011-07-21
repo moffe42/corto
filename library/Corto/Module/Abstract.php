eaf07ef9edab7c18972d8caffa0e1b722e0a03e5
relaxnownl <relaxnownl@gmail.com>
2011-07-21 08:59:50 +0000

    Made a tag for this version of Corto 0.2, calling it Corto v0.2.0 (semver compliant)

--- library/Corto/Module/Abstract.php
+++ library/Corto/Module/Abstract.php
@@ -6,19 +6,19 @@
  */
 abstract class Corto_Module_Abstract
 {
-	/**
-	 * A reference to the Corto_ProxyServer to which this module belongs.
-	 * @var Corto_ProxyServer
-	 */
-	protected $_server;
+    /**
+     * A reference to the Corto_ProxyServer to which this module belongs.
+     * @var Corto_ProxyServer
+     */
+    protected $_server;
 
-	/**
-	 * Construct a module, passing in a reference to the Corto_ProxyServer
-	 * to which this module belongs
-	 * @param Corto_ProxyServer $server
-	 */
-	public function __construct(Corto_ProxyServer $server)
-	{
-		$this->_server = $server;
-	}
+    /**
+     * Construct a module, passing in a reference to the Corto_ProxyServer
+     * to which this module belongs
+     * @param Corto_ProxyServer $server
+     */
+    public function __construct(Corto_ProxyServer $server)
+    {
+        $this->_server = $server;
+    }
 }
